9ed22f385ab47e63a1f48e2d4b66e6246838dd1a
dastangoo <hamed.dastangoo@gmail.com>
2017-06-12 21:59:11 +0430

    Just an initial commit which consists of some cleanig

--- components/scripts/tagline.js
+++ components/scripts/tagline.js
@@ -1,10 +1,7 @@
-(function() {
-  var fill;
+var fill;
 
-  (fill = function(item) {
-    return $('.tagline').append("" + item);
-  })('The most creative minds in Art');
+(fill = function(item) {
+  return $('.tagline').append("" + item);
+})('The most creative minds in Art');
 
-  fill;
-
-}).call(this);
+fill;
