4e939929caf06a9fe30e189b47ab188d126ca9ca
dastangoo <hamed.dastangoo@gmail.com>
2017-06-12 23:41:52 +0430

    added compass process

--- components/scripts/template.js
+++ components/scripts/template.js
@@ -1,9 +1,10 @@
- $(function() {
+$(function() {
   var Mustache = require('mustache');
-
+ 
   $.getJSON('js/data.json', function(data) {
     var template = $('#speakerstpl').html();
     var html = Mustache.to_html(template, data);
-   }); //getJSON
-
+    $('#speakers').html(html);    
+  }); //getJSON
+  
 }); //function
