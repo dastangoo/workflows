9363cb0c78ed93f23d487c7552a79d8da2c0c354
dastangoo <hamed.dastangoo@gmail.com>
2017-06-12 22:54:54 +0430

    Added Browserify

--- components/scripts/template.js
+++ components/scripts/template.js
@@ -1,10 +1,9 @@
-$(function() {
+ $(function() {
   var Mustache = require('mustache');
- 
+
   $.getJSON('js/data.json', function(data) {
     var template = $('#speakerstpl').html();
     var html = Mustache.to_html(template, data);
-    $('#speakers').html(html);    
-  }); //getJSON
-  
+   }); //getJSON
+
 }); //function
